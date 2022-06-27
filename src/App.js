9196b24809bfba03dc168cd04d8978551b857ab6
helany <helany.f.melo@gmail.com>
2022-06-26 22:51:56 -0300

    adding birthday project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import List from './List/List';
+import { useState } from 'react';
+import data from './data/data';
 
 function App() {
+  const [people, setPeople]=useState(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container'>
+      <h3>{people.length} birthday today</h3>
+      <List people={people}/>
+      <button onClick={()=>setPeople([])}>CLEAR</button>
+    </section>
   );
 }
 
